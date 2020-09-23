c9ca508f4fac9647b5e73f6a7dbc699a875fe2ac
Simon Knott <info@simonknott.de>
2020-09-23 14:37:53 +0200

    Make Invoke button interactive

--- development-ui/pages/pending.tsx
+++ development-ui/pages/pending.tsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import { Table } from "../components/Table";
 import { useQuirrel } from "../hooks/useQuirrel";
 import { BaseLayout } from "../layouts/BaseLayout";
 
-interface PendingJob {
-  endpoint: string;
-  id: string;
+interface InvokeButtonProps {
+  invoke(): Promise<void>;
+}
+
+function InvokeButton(props: InvokeButtonProps) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <button
+      className="bg-transparent hover:bg-indigo-500 text-indigo-700 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded w-20"
+      onClick={async () => {
+        setIsLoading(true);
+        await props.invoke();
+        setIsLoading(false);
+      }}
+    >
+      {isLoading ? "..." : "Invoke"}
+    </button>
+  );
 }
 
 export default function Pending() {
   const { pending, invoke } = useQuirrel();
   return (
     <BaseLayout selectedPage="pending">
-      <Table<PendingJob>
+      <Table
         items={pending}
         columns={[
           {
@@ -23,14 +40,7 @@ export default function Pending() {
             render: (job) => job.id,
           },
         ]}
-        endOfRow={(job) => (
-          <button
-            className="bg-transparent hover:bg-indigo-500 text-indigo-700 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded"
-            onClick={() => invoke(job)}
-          >
-            Invoke
-          </button>
-        )}
+        endOfRow={(job) => <InvokeButton invoke={() => invoke(job)} />}
       />
     </BaseLayout>
   );
