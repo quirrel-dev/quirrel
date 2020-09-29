8b6437fd4e80128bd29298eaaba27dba0abd116d
Simon Knott <info@simonknott.de>
2020-09-29 10:02:11 +0200

    Add Umami Tracking to dev ui

--- development-ui/pages/_app.tsx
+++ development-ui/pages/_app.tsx
@@ -1,11 +1,22 @@
 import { QuirrelProvider } from "../hooks/useQuirrel";
 import "../styles/tailwind.css";
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QuirrelProvider>
-      <Component {...pageProps} />
-    </QuirrelProvider>
+    <>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="9a40fe6e-8d93-45f4-b660-451d0ab28921"
+          src="https://umami.quirrel.dev/umami.js"
+        ></script>
+      </Head>
+      <QuirrelProvider>
+        <Component {...pageProps} />
+      </QuirrelProvider>
+    </>
   );
 }
 
