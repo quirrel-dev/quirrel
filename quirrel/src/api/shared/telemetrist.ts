ac66a0436720a594b89f6370bd1f8339ee59111a
Simon Knott <info@simonknott.de>
2020-12-23 11:07:49 +0100

    Use plausible-telemetry

--- quirrel/src/api/shared/telemetrist.ts
+++ quirrel/src/api/shared/telemetrist.ts
@@ -1,41 +1,16 @@
-import axios from "axios";
+import PlausibleTelemetry from "plausible-telemetry";
 
-// see https://github.com/plausible/analytics/blob/d463814da72f28b3323a520314fc52f195ed40ab/lib/plausible_web/controllers/api/external_controller.ex#L155
-// for a mapping between screenwidth and device
-enum ScreenWidth {
-  Docker = 500, // Mobile
-  Node = 900, // Tablet
-}
+new PlausibleTelemetry(
+  "telemetry.quirrel.dev",
+  "mobile"
+)
 
-export class Telemetrist {
-  constructor(private readonly runningInDocker: boolean) {}
-
-  private getScreenWidth() {
-    return this.runningInDocker ? ScreenWidth.Docker : ScreenWidth.Node;
+export class Telemetrist extends PlausibleTelemetry {
+  constructor(runningInDocker: boolean) {
+    super("telemetry.quirrel.dev", runningInDocker ? "mobile" : "tablet");
   }
 
   public async dispatch(path: string, name = "pageview") {
-    try {
-      await axios.post(
-        "https://plausible.io/api/event",
-        {
-          name,
-          url: `https://telemetry.quirrel.dev/${path}`,
-          domain: "telemetry.quirrel.dev",
-          screen_width: this.getScreenWidth(),
-        },
-        {
-          headers: {
-            "Content-Type": "text/plain",
-          },
-        }
-      );
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  public async goal(name: string) {
-    await this.dispatch("", name);
+    await this.record(path, name);
   }
 }
