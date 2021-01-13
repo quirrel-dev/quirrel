9035b14f806a053775a16441560d15e3afcd99d4
Simon Knott <info@simonknott.de>
2021-01-13 10:42:47 +0100

    Add test for invocation

---
+++ development-ui/test/invoke.spec.ts
@@ -0,0 +1,56 @@
+import delay from "delay";
+import { runQuirrel } from "./runQuirrel";
+import { expect } from "chai";
+
+let cleanup: (() => Promise<void>)[] = [];
+
+beforeEach(() => {
+  cleanup = [];
+});
+
+afterEach(async () => {
+  await Promise.all(cleanup.map((clean) => clean()));
+});
+
+async function expectTableCellToEqual(
+  row: number,
+  column: number,
+  value: string,
+  _page = page
+) {
+  const rowEl = await _page.$(`//tr[${row}]`);
+  expect(rowEl).to.exist;
+  expect(
+    await (await _page.$(`//tr[${row}]/td[${column}]`))?.innerText()
+  ).to.equal(value);
+}
+
+async function expectTableToBeEmpty(_page = page) {
+  const table = await _page.$(`tbody`);
+  expect(await table?.innerHTML()).to.equal("");
+}
+
+it.jestPlaywrightDebug("allows invoking jobs", async ({ page }) => {
+  const quirrel = await runQuirrel();
+  cleanup.push(quirrel.cleanup);
+
+  await page.goto("http://localhost:3000/pending");
+
+  await expectTableToBeEmpty(page);
+
+  await quirrel.client.enqueue("1", { delay: "1min", id: "1" });
+  await quirrel.client.enqueue("2", { delay: "2min", id: "2" });
+  await quirrel.client.enqueue("3", { delay: "3min", id: "3" });
+
+  await delay(100);
+
+  await expectTableCellToEqual(1, 2, "1", page);
+  await expectTableCellToEqual(2, 2, "2", page);
+  await expectTableCellToEqual(3, 2, "3", page);
+
+  await page.click(`//tr[1]/td[4]/span/button`);
+
+  await delay(100);
+
+  expect(quirrel.receivedJobs).to.contain("1");
+});
--- development-ui/test/runQuirrel.ts
+++ development-ui/test/runQuirrel.ts
@@ -1,5 +1,6 @@
-import { runQuirrel as _runQuirrel } from "quirrel";
+import { QuirrelClient, runQuirrel as _runQuirrel } from "quirrel";
 import IORedis from "ioredis-mock";
+import http from "http";
 
 export async function runQuirrel({ port = 9181 }: { port?: number } = {}) {
   const ioredis = new IORedis();
@@ -11,7 +12,44 @@ export async function runQuirrel({ port = 9181 }: { port?: number } = {}) {
     },
   });
 
+  const receivedJobs: string[] = [];
+
+  const client = new QuirrelClient<string>({
+    async handler(payload) {
+      receivedJobs.push(payload);
+    },
+    route: "",
+    config: {
+      applicationBaseUrl: "http://localhost:5000",
+      quirrelBaseUrl: "http://localhost:" + port,
+    },
+  });
+
+  const receiver = http
+    .createServer((req, res) => {
+      let body = "";
+      req.on("data", (data) => {
+        body += data;
+      });
+
+      req.on("end", async () => {
+        const response = await client.respondTo(body, req.headers);
+        res.statusCode = response.status;
+        for (const [name, value] of Object.entries(response.headers)) {
+          res.setHeader(name, value);
+        }
+        res.write(body);
+        res.end();
+      });
+    })
+    .listen(5000);
+
   return {
-    cleanup: quirrelServer.close,
+    client,
+    receivedJobs,
+    async cleanup() {
+      await quirrelServer.close();
+      receiver.close();
+    },
   };
 }
