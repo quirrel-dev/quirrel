8b79c26b546b4cd4a168a7b70d66442c62522ccf
Simon Knott <info@simonknott.de>
2021-01-21 17:30:55 +0100

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -133,7 +133,7 @@ module.exports = {
         docs: {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/quirrel-dev/quirrel/edit/master/docs/",
+          editUrl: "https://github.com/quirrel-dev/quirrel/edit/main/docs/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
