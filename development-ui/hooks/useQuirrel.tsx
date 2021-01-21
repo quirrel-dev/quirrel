150c25e42a10bf7f728c8888c947e0cc9c006896
Simon Knott <info@simonknott.de>
2021-01-21 13:19:24 +0100

    Stop polling after alert

--- development-ui/hooks/useQuirrel.tsx
+++ development-ui/hooks/useQuirrel.tsx
@@ -285,18 +285,21 @@ function useQuirrelClient() {
   };
 }
 
-async function isHealthy(baseUrl: string) {
+async function isHealthy(
+  baseUrl: string
+): Promise<{ isHealthy: boolean; stopPolling?: boolean }> {
   try {
     const res = await fetch(baseUrl + "/health");
-    return res.status === 200;
+    return { isHealthy: res.status === 200 };
   } catch (error) {
     if (error.message === "Not allowed to request resource") {
       window.alert(
         "This browser does not support connecting to your local Quirrel instance. Please use a different browser."
       );
+      return { isHealthy: false, stopPolling: true };
     }
 
-    return false;
+    return { isHealthy: false };
   }
 }
 
@@ -381,14 +384,21 @@ export function QuirrelProvider(props: PropsWithChildren<{}>) {
 
   const connect = useCallback(
     async (instanceDetails: QuirrelInstanceDetails) => {
-      if (!(await isHealthy(instanceDetails.baseUrl))) {
-        return;
+      const { isHealthy: _isHealthy, stopPolling } = await isHealthy(
+        instanceDetails.baseUrl
+      );
+      if (stopPolling) {
+        return "stopPolling";
+      }
+      if (!_isHealthy) {
+        return "unhealthy";
       }
 
       const getClient = quirrelClient.useInstance(instanceDetails);
 
       await loadInitialJobs(getClient);
       connectActivityStream(instanceDetails);
+      return "success";
     },
     [quirrelClient.useInstance, loadInitialJobs, connectActivityStream]
   );
@@ -398,10 +408,14 @@ export function QuirrelProvider(props: PropsWithChildren<{}>) {
       return;
     }
 
-    const intervalId = setInterval(() => {
-      connect({
+    const intervalId = setInterval(async () => {
+      const result = await connect({
         baseUrl: "http://localhost:9181",
       });
+
+      if (result === "stopPolling" || result === "success") {
+        clearInterval(intervalId);
+      }
     }, 500);
 
     return () => clearInterval(intervalId);
