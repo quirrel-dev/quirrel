aaa52e62613c68990d2da2a49958793e5393b946
Simon Knott <info@simonknott.de>
2021-03-08 09:39:54 +0100

    Implement

---
+++ quirrel/src/client/enhanced-json.ts
@@ -0,0 +1,15 @@
+import SuperJSON from "superjson";
+
+export function stringify(value: any): string {
+  return SuperJSON.stringify(value);
+}
+
+export function parse(string: string): any {
+  const parsedJSON = JSON.parse(string);
+
+  if (typeof parsedJSON === "object" && parsedJSON.json) {
+    return SuperJSON.deserialize(parsedJSON);
+  }
+
+  return parsedJSON;
+}
