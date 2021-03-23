adac2472c6d801242a346c58540817f8e852cfbb
Simon Knott <info@simonknott.de>
2021-03-23 15:19:41 +0100

    adapt test

--- quirrel/src/client/test/dx.test.ts
+++ quirrel/src/client/test/dx.test.ts
@@ -11,10 +11,34 @@ describe("DX", () => {
     },
   });
 
-  it("defaults endpoints to https, if no protocol given", async () => {
-    await expect(() => quirrel.enqueue("")).rejects.toThrowError(
-      "request to https://quirrel.mock.com/queues/https%3A%2F%2Fanysite.com%2Fapi%2FsomeAPI failed, reason: getaddrinfo ENOTFOUND quirrel.mock.com"
-    );
+  describe("if no protocol given", () => {
+    describe("in dev mode", () => {
+      it("defaults endpoints to http", async () => {
+        await expect(() => quirrel.enqueue("")).rejects.toThrowError(
+          "request to https://quirrel.mock.com/queues/http%3A%2F%2Fanysite.com%2Fapi%2FsomeAPI failed, reason: getaddrinfo ENOTFOUND quirrel.mock.com"
+        );
+      });
+    });
+
+    describe("in prod mode", () => {
+      it("defaults endpoints to https", async () => {
+        const oldEnv = process.env.NODE_ENV;
+        process.env.NODE_ENV = "production";
+        const quirrel = new QuirrelClient({
+          async handler() {},
+          route: "api/someAPI",
+          config: {
+            quirrelBaseUrl: "https://quirrel.mock.com",
+            applicationBaseUrl: "anysite.com",
+            encryptionSecret: "4ws8syoOgeQX6WFvXuUneGNwy7QvLxpk",
+          },
+        });
+        await expect(() => quirrel.enqueue("")).rejects.toThrowError(
+          "request to https://quirrel.mock.com/queues/https%3A%2F%2Fanysite.com%2Fapi%2FsomeAPI failed, reason: getaddrinfo ENOTFOUND quirrel.mock.com"
+        );
+        process.env.NODE_ENV = oldEnv;
+      });
+    });
   });
 
   describe("when given a runAt in the past", () => {
