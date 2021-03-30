1a3119745ed922ed35d0e45b5e56d3838e9d9ebb
Simon Knott <info@simonknott.de>
2021-03-30 10:16:39 +0200

    Place twitter in landing page

--- quirrel/src/api/scheduler/routes/index.ts
+++ quirrel/src/api/scheduler/routes/index.ts
@@ -1,6 +1,8 @@
 import { FastifyPluginCallback } from "fastify";
 
 const welcomePage = `
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+
 <h1>
 Welcome to the Quirrel API!
 </h1>
@@ -25,6 +27,10 @@ As an end-user, you're most likely looking for something else:
 If you didn't get here by accident, but want to interface directly with the Quirrel API,
 take a look at the <a href="/documentation">OpenAPI spec</a>.
 </p>
+
+<footer>
+  Built with &#10084; by <a href="https://twitter.com/skn0tt">@skn0tt</a>.
+</footer>
 `.trim();
 
 const index: FastifyPluginCallback = (fastify, opts, done) => {
