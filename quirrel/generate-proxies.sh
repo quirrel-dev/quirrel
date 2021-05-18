8b81bf137e2af4d3ca013ffed08b9ede8aea0446
Simon Knott <info@simonknott.de>
2021-05-18 11:15:12 +0200

    Link client.js to "client/index.js"

--- quirrel/generate-proxies.sh
+++ quirrel/generate-proxies.sh
@@ -6,9 +6,14 @@ function generate_proxy() {
   withoutExtension=$(echo $file | cut -f 1 -d '.')
   echo "module.exports = require('./dist/cjs/src/$withoutExtension');" > $withoutExtension.cjs
   cp $withoutExtension.cjs $withoutExtension.js
-  echo "export * from './dist/esm/src/$withoutExtension.js'
-export { default } from './dist/esm/src/$withoutExtension.js'" > $withoutExtension.d.ts
-  echo "export * from './dist/esm/src/$withoutExtension.js'" > $withoutExtension.mjs
+
+  withExtension="$withoutExtension.js"
+  if [ $withoutExtension == 'client' ]; then
+		withExtension="client/index.js"
+	fi
+  echo "export * from './dist/esm/src/$withExtension'
+export { default } from './dist/esm/src/$withExtension'" > $withoutExtension.d.ts
+  echo "export * from './dist/esm/src/$withExtension'" > $withoutExtension.mjs
 }
 
 for file in $(ls -p src | grep -v /)
